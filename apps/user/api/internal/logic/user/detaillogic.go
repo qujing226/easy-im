4ea6d3bccb73dca9a826289408efb0b01120b864
peninsula12 <qujing226@gmail.com>
2024-09-30 20:34:02 +0800

    api

---
+++ apps/user/api/internal/logic/user/detaillogic.go
@@ -0,0 +1,47 @@
+package user
+
+import (
+	"context"
+	"easy-chat/apps/user/rpc/user"
+	"easy-chat/pkg/ctxdata"
+	"github.com/jinzhu/copier"
+
+	"easy-chat/apps/user/api/internal/svc"
+	"easy-chat/apps/user/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type DetailLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// NewDetailLogic 获取用户信息
+func NewDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DetailLogic {
+	return &DetailLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *DetailLogic) Detail(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
+	// todo: add your logic here and delete this line
+	uid := ctxdata.GetUId(l.ctx)
+	userInfoResp, err := l.svcCtx.User.GetUserInfo(l.ctx, &user.GetUserInfoReq{
+		User: uid,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var res types.User
+	err = copier.Copy(&res, userInfoResp.User)
+	if err != nil {
+		return nil, err
+	}
+	resp = &types.UserInfoResp{Info: res}
+	return
+}
