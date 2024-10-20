81df9b9c3b3f149a3c1592797e20a4ffd4f169c5
peninsula12 <qujing226@gmail.com>
2024-10-20 20:07:23 +0800

    solve

---
+++ deploy/script/social-rpc-dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+reso_addr="crpi-4ll7w1qk44676hc7.cn-hangzhou.personal.cr.aliyuncs.com/go-chat-im/social-rpc-dev"
+tag="latest"
+container_name="go-chat-im-social-rpc-dev"
+
+username="Peninsular"
+password="yujie2018"
+
+echo $password | docker login $reso_addr -u $username --password-stdin
+
+# 停止容器
+docker stop ${container_name} || true
+# 删除容器
+docker rm ${container_name} || true
+# 检查镜像是否存在，如果存在则删除
+if docker images | grep -q "${reso_addr}:${tag}"; then
+  docker rmi ${reso_addr}:${tag}
+fi
+# 拉取最新的镜像
+docker pull ${reso_addr}:${tag}
+# 运行新的容器
+docker run -d --name ${container_name} --network go-im_easy-chat  -p 10000:10000 ${reso_addr}:${tag}
